f5d410a216616ba86943532300632cf6a23a701f
jatinder <jatindersingh@ucreate.co.in>
2018-12-12 18:08:53 +0530

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel App
+                    Laravel App1
                 </div>
 
                 <div class="links">
